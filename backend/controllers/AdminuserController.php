2007f9a97202cc04b4cef792cc9b83b79d434d5d
panchaozhi <1099958338@qq,com>
2017-02-28 23:59:02 +0800

    获取角色和权限

--- backend/controllers/AdminuserController.php
+++ backend/controllers/AdminuserController.php
@@ -2,12 +2,17 @@
 
 namespace backend\controllers;
 
+use backend\models\ResetPasswordForm;
+use common\models\AuthAssignment;
+use common\models\AuthItem;
 use Yii;
 use common\models\Adminuser;
 use common\models\AdminuserSearch;
+use yii\rbac\Assignment;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use backend\models\SignupForm;
 
 /**
  * AdminuserController implements the CRUD actions for Adminuser model.
@@ -63,10 +68,12 @@ class AdminuserController extends Controller
      */
     public function actionCreate()
     {
-        $model = new Adminuser();
+        $model = new SignupForm();
 
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+        if ($model->load(Yii::$app->request->post())) {
+            if ($user = $model->signup()) {
+                return $this->redirect(['view', 'id' => $user->id]);
+            }
         } else {
             return $this->render('create', [
                 'model' => $model,
@@ -118,7 +125,91 @@ class AdminuserController extends Controller
         if (($model = Adminuser::findOne($id)) !== null) {
             return $model;
         } else {
-            throw new NotFoundHttpException('The requested page does not exist.');
+            throw new NotFoundHttpException('请求页面不存在！.');
         }
+    }
+
+    public function actionResetpwd($id)
+    {
+        $model = new ResetPasswordForm();
+        if ($model->load(Yii::$app->request->post())) {
+            if ($model->resetPassword($id)) {
+                return $this->redirect(['index']);
+            }
+        }
+        return $this->render('resetpwd', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * 获取角色和权限
+     * @param $id
+     * @return string|\yii\web\Response
+     */
+    public function actionPrivilege($id)
+    {
+        // step1. 找出所有的角色，提供给 checkboxlist。type=1是角色，type=2是角色拥有的权限
+        $allPrivileges = AuthItem::find()
+            ->select(['name', 'description'])
+            ->where(['type' => 1])
+            ->orderBy('description')
+            ->all();
+        //zhi($allPrivileges);
+        $allPrivilegesArray = [];
+        foreach ($allPrivileges as $privilege) {
+            $allPrivilegesArray[$privilege->name] = $privilege->description;
+        }
+        //zhi($allPrivilegesArray); 打印结果：
+        /* Array
+         * (
+         *   [postOperator] => 文章操作员
+         *   [postAdmin] => 文章管理员
+         *   [admin] => 系统管理员
+         *   [commentAuditor] => 评论审核员
+         * )
+         *
+         */
+
+        // step2. 获取当前用户的角色的权限，点进权限的时候已经选择的复选框
+        $allAssignments = AuthAssignment::find()
+            ->select(['item_name'])
+            ->where(['user_id' => $id])
+            ->orderBy('item_name')
+            ->all();
+        // zhi($allAssignment);
+        // array_push() 将一个或多个单元压入数组的末尾（入栈）
+        $AuthAssignmentArray = [];
+        foreach ($allAssignments as $assignment) {
+            array_push($AuthAssignmentArray, $assignment->item_name);
+        }
+        //zhi($AuthAssignmentArray); [0] => admin
+
+        // step3. 获取从表单提交的数据，来更新 AuthAssignment 表，从而用户的角色发生了变化
+        if (isset($_POST['privilege'])) {
+            AuthAssignment::deleteAll('user_id=:id', [':id' => $id]);
+
+            $privilege = $_POST['privilege'];
+            $arrlength = count($privilege);
+            //zhi($privilege);exit;
+
+            for ($x = 0; $x < $arrlength; $x++) {
+                $newPri = new AuthAssignment();
+                $newPri->item_name = $privilege[$x];
+                $newPri->user_id = $id;
+                $newPri->created_at = time();
+
+                $newPri->save();
+            }
+
+            return $this->redirect(['index']);
+        }
+
+        // step4. 渲染 checkboxlist 表单
+        return $this->render('privilege', [
+            'id' => $id,
+            'allPrivilegesArray' => $allPrivilegesArray,
+            'AuthAssignmentArray' => $AuthAssignmentArray,
+        ]);
     }
 }
