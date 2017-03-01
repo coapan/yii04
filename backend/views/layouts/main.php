9c095c73837cb2ac7fe6be37eedfe19925468274
panchaozhi <1099958338@qq,com>
2017-03-01 20:48:25 +0800

    添加评论数量气泡

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -9,6 +9,7 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use common\widgets\Alert;
+use common\models\Comment;
 
 AppAsset::register($this);
 ?>
@@ -40,14 +41,15 @@ AppAsset::register($this);
         ['label' => '文章管理', 'url' => ['/post/index']],
         ['label' => '用户管理', 'url' => ['/user/index']],
         ['label' => '评论管理', 'url' => ['/comment/index']],
+        '<li><span class="badge">'.Comment::getCommentCount().'</span></li>',
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
     } else {
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
+                '注销 (' . Yii::$app->user->identity->username . ')',
                 ['class' => 'btn btn-link logout']
             )
             . Html::endForm()
